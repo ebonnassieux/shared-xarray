d020ccb3b81226ccf43de8a07cf7b0355a7bc1b4
Etienne Bonnassieux <ebonnassieux@synchrotron.localdomain>
2025-12-01 15:27:33 +0100

    Should be functional

--- src/shared_xarray/shared_xarray.py
+++ src/shared_xarray/shared_xarray.py
@@ -8,6 +8,13 @@ import numpy as np
 from uuid import uuid4
 from xarray import DataArray
 
+# build global list of shared-xarrays
+if "DicoSharedXarrays" not in globals().keys():
+  global DicoSharedXarrays
+  DicoSharedXarrays={}
+  DicoSharedXarrays["Name"]={}
+  DicoSharedXarrays["Name"]["Closer"]=[]
+
 # define shared memory closer function
 def close_shm(name: str):
   """Cleanup shared memory"""
@@ -32,14 +39,12 @@ def unlink_shm(name: str):
   finally:
     shm.close()
 
-
 class SharedArray():
   """
   docstring here
   """
 
-  def __getitem__(self, index):
-    print("Getting vals associated with index",index)
+  def __getitem__(self, index):    
     return self._array[index]
   
   def __setitem__(self,index,newvalue):
@@ -48,32 +53,36 @@ class SharedArray():
   def __str__(self):
     return self._array.values.__str__()
 
-### figure out the below
-#  def __repr__(self):
-#    return "SharedXarray("+self._array.values.__str__()+")"
+  def __repr__(self):
+    return f"SharedXarray(name={self.name}, {self._array.values.__str__()})"
 
+  # below, ro means readonly
   def __init__(self,
                name_str           : str,
                shape              : Tuple[int, ...],
                dtype              : npt.DTypeLike=float,
-               writeable          : bool=True,
+               ro                 : bool=False,
                verbose            : bool=False,
                ms_async_flag      : bool=False,
                ms_sync_flag       : bool=False,
                ms_invalidate_flag : bool=False,
                op                 : str = "Create"
                ):
+    ### todo, implement readonly
+    if ro==True:
+      print("Readonly mode not supported yet")
+      return NotImplemented
     # set flags
     self.MS_ASYNC=ms_async_flag
     self.MS_SYNC=ms_sync_flag
     self.MS_INVALIDATE=ms_invalidate_flag
     # parse input string to replicate SharedArray behaviour
     if "//" in name_str:
-      prefix,name = name_str.split("//")
+      self.prefix,self.name = name_str.split("//")
     else:
-      prefix=""
-      name=name_str
-    if prefix== "file:":
+      self.prefix=""
+      self.name=name_str
+    if self.prefix== "file:":
       ### TODO implement file backend
       print("File backend not supported yet")
       return NotImplemented
@@ -85,31 +94,31 @@ class SharedArray():
         nbytes = int(np.prod(shape)) * dt.itemsize
         self.arr_shape=shape
         try:
-          self._shm = SharedMemory(create=True, size=nbytes, name=name)
-          finalize_fn = unlink_shm
+          self._shm = SharedMemory(create=True, size=nbytes, name=self.name)
+#          finalize_fn = unlink_shm
         except FileExistsError:
           if verbose:
             print("Shared memory file already exists")
           return
         # set array value.
         self._array = DataArray(np.ndarray(shape, dt, self._shm.buf))
+        self._shm.shared_xarray_shape = shape
+        self._shm.shared_xarray_dtype = dtype
         #self._array.setflags(write=writeable)
         if verbose==True:
           print(f"{op} {self} in {os.getpid()}")
+        # add params to global variable
+#        DicoSharedXarrays[self.name]=finalize_fn
       # attach if requested.
       elif op=="Attach":
         # attach if already existing
         try:
-          self._shm = SharedMemory(create=False, name=name)
+          self._shm = SharedMemory(create=False, name=self.name)
+          array = np.frombuffer(self._shm.buf, dtype)
+          shape=array.shape
           dt = np.dtype(dtype)
-          ##### THE PROBLEM RIGHT NOW IS CORRECTLY INHERITING
-          ##### THE SHAPE WHEN ATTACHING. OTHERWISE, IT WORKS. 
-#          shape = self._shm.buf.size//dt.itemsize
-          print(shape)
-          shape=[]
           self._array = DataArray(np.ndarray(shape,dt,self._shm.buf))
-          #assert self._shm.buf.nbytes == nbytes
-          finalize_fn = close_shm
+#          finalize_fn = close_shm
         except FileNotFoundError:
           if verbose:
             print("Shared memory file does not yet exist")
@@ -118,12 +127,12 @@ class SharedArray():
           if verbose:
             print("Operation not supported")
             return NotImplemented
-#    print(self._shm.buf)
-#    stop
+        # add params to global variable
+#        DicoSharedXarrays[self.name]=finalize_fn
     # set array value
     self._array = DataArray(np.ndarray(shape, dt, self._shm.buf))
     # define weakref for clean bookkeeping purposes
-    weakref.finalize(self, finalize_fn, self._shm.name)
+#    weakref.finalize(self, finalize_fn, self._shm.name)
     self._verbose = verbose
     if verbose==True:
       print(f"{op} {self} in {os.getpid()}")
@@ -133,29 +142,52 @@ class SharedArray():
   @classmethod
   def create(cls,
              name,
-             shape):
-    return cls(name, shape, op="Create")
+             shape,
+             ro=False):
+    return cls(name, shape, op="Create",ro=ro)
 
   # make class attachment method to ensure .attach() call
   # mirrors reference SharedArray behaviour
   @classmethod
   def attach(cls,
-             name):
-    return cls(name,shape=[], op="Attach")
+             name,
+             ro=False):
+    # read shape from the input array name
+    return cls(name,shape=[], op="Attach",ro=ro)
 
   @property
   def shape(self) -> Tuple[int, ...]:
     return self._array.shape
+
+  @property
+  def values(self) -> array:
+    return self._array.values
+
+
+
+######## delete functions
   
-  def delete(self,name):
-    """
-    docstring here
-    """
-    return
   
+  @classmethod
+  def delete(cls,
+             name):
+    """
+    Unlink shared memory
+    """
+    try:
+      shm = SharedMemory(name)
+    except FileNotFoundError:
+      return
+    try:
+      shm.unlink()
+    except Exception:
+      print(f"Unlinking {name} failed")
+    finally:
+      shm.close()
+
 
   def list(self):
     """
     docstring here
     """
-    return
+    return NotImplemented
